b0fa9f846f8968fd63f17b5c12bd371590ecfd2f
nutan654 <149276841+nutan654@users.noreply.github.com>
2024-07-15 23:27:06 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,64 @@
+
+let allMusic= [
+  {
+    name: "There's Nothing Holdin' Me Back",
+    artist: "Shawn Mendes",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Treat You Better",
+    artist: "Shawn Mendes",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Payphone",
+    artist: "Maroon 5",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Sugar",
+    artist: "Robin Schulz",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Thinking Out Loud",
+    artist: "Ed sheeran",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Marvin Gaye",
+    artist: "Charlie Puth",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "7 Years",
+    artist: "Lukas Graham",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Titanium",
+    artist: "David Guetta",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Despacito",
+    artist: "Luis Fonsi & Daddy Yankee",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Galway Girl",
+    artist: "Ed Sheeran",
+    img: "music-10",
+    src: "music-10"
+  },
+  
+];
